7acad17ec2642811715324b2dc8f90a7a6e6fadf
Denis Androic <denis.androic@and.digital>
2021-09-21 16:21:28 +0200

    feat: re-enable aws deployment
    Co-authored-by: Richard Corke <sandriches@users.noreply.github.com>

--- src/mollieClient.ts
+++ src/mollieClient.ts
@@ -1,24 +1,23 @@
-// import { createMollieClient } from '@mollie/api-client';
-// import { loadConfig } from '../config/config';
+import { createMollieClient } from '@mollie/api-client';
+import { loadConfig } from '../config/config';
 
-// let config = loadConfig();
+let config = loadConfig();
 
-// let testApiKey: string;
-// try {
-//   testApiKey = config.MOLLIE_TEST_API_KEY;
-// } catch (err) {
-//   throw new Error('Set env config file');
-// }
+let testApiKey: string;
+try {
+  testApiKey = config.MOLLIE_TEST_API_KEY;
+} catch (err) {
+  throw new Error('Set env config file');
+}
 
-// const mollieClient = createMollieClient({ apiKey: testApiKey });
+const mollieClient = createMollieClient({ apiKey: testApiKey });
 
 
 exports.handler = async () => {
-  console.log('Works yay')
   // Methods for the Payments API
-  // let methods = await mollieClient.methods.all();
+  let methods = await mollieClient.methods.all();
 
   // Methods for the Orders API
-  // methods = await mollieClient.methods.all({ resource: 'orders' });
-  return [];
+  methods = await mollieClient.methods.all({ resource: 'orders' });
+  return methods;
 };
