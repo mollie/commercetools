6399d75f84eb8c3fa1c3ab8db3338c500393fa66
lebradley <lbradley@and.digital>
2021-10-07 17:57:27 +0200

    fix: remove unused code

--- notifications/gcpFunctionHandler.ts
+++ notifications/gcpFunctionHandler.ts
@@ -1,22 +1,12 @@
 import { Request, Response } from 'express'
 import config from './config/config'
 import createMollieClient, { List, Method } from '@mollie/api-client'
-import { processAction } from './src/requestHandlers/handleRequest'
 
 const mollieApiKey = config.mollieApiKey
 
 const mollieClient = createMollieClient({ apiKey: mollieApiKey })
 
 exports.handler = async (req: Request, res: Response) => {
-
-  // validate/get action
-  // const action:  = validateAction(request...)
-  const action: string | undefined = 'getPaymentMethods'
-  // error if unknown action
-  if (!action) {
-    // return error response
-  }
-
   try {
     res.status(200).send('Ok')
   } catch (error: any) {
