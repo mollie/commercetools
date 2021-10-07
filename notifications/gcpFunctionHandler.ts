231472af0b748da68bacab1faaa8bd427db7a827
lebradley <lbradley@and.digital>
2021-10-07 18:33:05 +0200

    feat: simplify handler

--- notifications/gcpFunctionHandler.ts
+++ notifications/gcpFunctionHandler.ts
@@ -8,7 +8,7 @@ const mollieClient = createMollieClient({ apiKey: mollieApiKey })
 
 exports.handler = async (req: Request, res: Response) => {
   try {
-    res.status(200).send('Ok')
+    res.status(200).end()
   } catch (error: any) {
     console.warn(error)
     res.status(400).send(error.message)
--- notifications/lambdaHandler.ts
+++ notifications/lambdaHandler.ts
@@ -1,13 +1,9 @@
+import { Request, Response } from 'express'
 import { createMollieClient } from '@mollie/api-client';
 import config from './config/config';
 
 const mollieClient = createMollieClient({ apiKey: config.mollieApiKey });
 
-exports.handler = async () => {
-  // Methods for the Payments API
-  let methods = await mollieClient.methods.all();
-
-  // Methods for the Orders API
-  methods = await mollieClient.methods.all({ resource: 'orders' });
-  return methods;
+exports.handler = async (req: Request, res: Response) => {
+  res.status(200).end()
 };
