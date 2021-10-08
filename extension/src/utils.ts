6fdc30ec4048597b730786f965a5601e7bedd252
Denis Androic <denis.androic@and.digital>
2021-10-08 16:49:41 +0200

    feat: add method list mapper and tests for it

--- extension/src/utils.ts
+++ extension/src/utils.ts
@@ -1,7 +1,43 @@
+import { MethodsListParams } from '@mollie/api-client';
 /**
  * Generates an ISO string date
  * @returns {String} Returns the current date converted to ISO.
  */
 export function createDateNowString() {
   return new Date().toISOString();
+}
+
+export function methodListMapper(ctObj: any): MethodsListParams {
+  // Generally this shouldn't be needed, but a safety anyway.. eventually could return error here
+  if (!ctObj.amountPlanned) {
+    return {}
+  }
+  const { currencyCode, centAmount, fractionDigits } = ctObj.amountPlanned
+  const divider = Math.pow(10, fractionDigits || 2)
+  const mollieAmount = (centAmount / divider).toFixed(2)
+
+  const mObject: MethodsListParams = {
+    amount: {
+      value: `${mollieAmount}`,
+      currency: currencyCode,
+    },
+    // Resource is hardcoded, for the time being we only support Orders API
+    resource: 'orders',
+  }
+
+  if (ctObj.custom?.fields?.paymentMethodsRequest) {
+    const { locale, billingCountry, includeWallets, orderLineCategories, issuers, pricing, sequenceType } = ctObj.custom?.fields?.paymentMethodsRequest
+    const include = issuers || pricing ? `${issuers ? 'issuers,' : ''}${pricing ? 'pricing' : ''}` : undefined
+
+    Object.assign(mObject,
+      locale && { locale: locale },
+      include && { include: include },
+      includeWallets && { includeWallets: includeWallets },
+      billingCountry && { billingCountry: billingCountry },
+      sequenceType && { sequenceType: sequenceType },
+      orderLineCategories && { orderLineCategories: orderLineCategories }
+    )
+  }
+
+  return mObject
 }
---
+++ extension/tests/utils.test.ts
@@ -0,0 +1,77 @@
+import * as ut from '../src/utils';
+
+describe('Utils unit test', () => {
+  describe('methodListMapper', () => {
+    it('Should return empty object if no amountPlanned / use as list all', async () => {
+      const mollieOptions = ut.methodListMapper({})
+      expect(mollieOptions).toMatchObject({});
+    });
+
+    it('Should return properly formated amount for molie', async () => {
+      const ctObj = {
+        amountPlanned: {
+          currencyCode: "USD",
+          centAmount: 1234,
+        }
+      }
+
+      const mollieOptions = ut.methodListMapper(ctObj)
+      expect(mollieOptions.amount).toHaveProperty('value', '12.34')
+      expect(mollieOptions.amount).toHaveProperty('currency', 'USD');
+    });
+
+    it('Should return properly formated custom fields for molie', async () => {
+      const ctObj = {
+        amountPlanned: {
+          currencyCode: "EUR",
+          centAmount: 100987,
+          fractionDigits: 4
+        },
+        custom: {
+          fields: {
+            paymentMethodsRequest: {
+              locale: "nl_NL",
+              billingCountry: "NL",
+              includeWallets: "applepay",
+              orderLineCategories: "eco,meal",
+              issuers: false,
+              pricing: false
+            }
+          }
+        }
+      }
+
+      const mollieOptions = ut.methodListMapper(ctObj)
+      expect(mollieOptions.amount).toHaveProperty('value', '10.10')
+      expect(mollieOptions.amount).toHaveProperty('currency', 'EUR');
+      expect(mollieOptions).toHaveProperty('locale', 'nl_NL');
+      expect(mollieOptions).toHaveProperty('billingCountry', 'NL');
+      expect(mollieOptions).toHaveProperty('includeWallets', 'applepay');
+      expect(mollieOptions).toHaveProperty('orderLineCategories', 'eco,meal');
+      expect(mollieOptions.include).toBeUndefined();
+    });
+
+    it('Should properly parse includes and not have ', async () => {
+      const ctObj = {
+        amountPlanned: {
+          currencyCode: "EUR",
+          centAmount: 1000,
+        },
+        custom: {
+          fields: {
+            paymentMethodsRequest: {
+              locale: "nl_NL",
+              issuers: false,
+              pricing: true
+            }
+          }
+        }
+      }
+
+      const mollieOptions = ut.methodListMapper(ctObj)
+      expect(mollieOptions).toHaveProperty('locale', 'nl_NL');
+      expect(mollieOptions).toHaveProperty('include', 'pricing');
+      expect(mollieOptions.billingCountry).toBeUndefined();
+    });
+  });
+});
