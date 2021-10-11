348b0e6de682bc90ca178a544c3a050514bd4ab3
Denis Androic <denis.androic@and.digital>
2021-10-11 14:12:01 +0200

    docs: add some description for validateAction

--- extension/src/requestHandlers/actions.ts
+++ extension/src/requestHandlers/actions.ts
@@ -3,6 +3,12 @@ export default {
   getPaymentMethods,
 };
 
+/**
+ * validateAction expects CT formatted body with action (and later custom params)
+ * @param body 
+ * Based on logic it then returns either an action string or undefined if no action could be determined
+ * @returns 'actionName'
+ */
 export function validateAction(body: any): string | undefined {
   let action = undefined;
   switch (true) {
