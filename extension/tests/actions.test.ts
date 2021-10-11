16ab431e8b407d9f5a5b958bbfeee8406e37316a
Denis Androic <denis.androic@and.digital>
2021-10-11 22:03:12 +0200

    feat: make getPaymentMethods run on update payment, test for action

---
+++ extension/tests/actions.test.ts
@@ -0,0 +1,28 @@
+import { validateAction } from '../src/requestHandlers/actions';
+
+describe('validateAction', () => {
+  it('Should return getPaymentMethods when payment is created (action = Create)', () => {
+    const mockReqBody = {
+      action: 'Create'
+    }
+    const action = validateAction(mockReqBody)
+    expect(action).toBe('getPaymentMethods');
+  });
+
+  it('Should return getPaymentMethods when payment is updated', () => {
+    const mockReqBody = {
+      action: 'Update',
+      resource: {
+        obj: {
+          custom: {
+            fields: {
+              paymentMethodsRequest: "{}"
+            }
+          }
+        }
+      }
+    }
+    const action = validateAction(mockReqBody)
+    expect(action).toBe('getPaymentMethods');
+  });
+})
