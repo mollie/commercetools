7a6d9f85522ae2ee0dcede406b6d7758d2d2fced
Denis Androic <denis.androic@and.digital>
2021-11-03 13:41:48 +0100

    test: add tests for processAction

--- extension/tests/requestHandlers/handleRequest.test.ts
+++ extension/tests/requestHandlers/handleRequest.test.ts
@@ -1,11 +1,13 @@
+import createMollieClient, { MollieClient } from '@mollie/api-client';
 import { Request, Response } from 'express';
 import { mocked } from 'ts-jest/utils';
 import actions, { validateAction } from '../../src/requestHandlers/actions';
-import handleRequest from '../../src/requestHandlers/handleRequest';
+import handleRequest, { processAction } from '../../src/requestHandlers/handleRequest';
 import { ControllerAction } from '../../src/types/index';
 import Logger from '../../src/logger/logger';
 
 jest.mock('../../src/requestHandlers/actions');
+jest.mock('@mollie/api-client');
 
 describe('handleRequest', () => {
   const req = {} as Request;
@@ -100,5 +102,46 @@ describe('handleRequest', () => {
       ],
     });
     expect(mockLogWarn).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('processActions', () => {
+  it('should call GetPaymentMethods if the action is GetPaymentMethods', async () => {
+    const mockedGetPaymentMethods = mocked(actions.getPaymentMethods);
+    await processAction(ControllerAction.GetPaymentMethods, {}, {} as MollieClient);
+    expect(mockedGetPaymentMethods).toBeCalledTimes(1);
+  });
+  it('should call CreateOrder if the action is CreateOrder', async () => {
+    const mockedCreateOrder = mocked(actions.createOrder);
+    await processAction(ControllerAction.CreateOrder, {}, {} as MollieClient);
+    expect(mockedCreateOrder).toBeCalledTimes(1);
+  });
+  it('should call CreateOrderPayment if the action is CreateOrderPayment', async () => {
+    const mockedCreateOrderPayment = mocked(actions.createOrderPayment);
+    await processAction(ControllerAction.CreateOrderPayment, {}, {} as MollieClient);
+    expect(mockedCreateOrderPayment).toBeCalledTimes(1);
+  });
+  it('should call CreateShipment if the action is CreateShipment', async () => {
+    const mockedCreateShipment = mocked(actions.createShipment);
+    await processAction(ControllerAction.CreateShipment, {}, {} as MollieClient);
+    expect(mockedCreateShipment).toBeCalledTimes(1);
+  });
+  it('should call UpdateShipment if the action is UpdateShipment', async () => {
+    const mockedUpdateShipment = mocked(actions.updateShipment);
+    await processAction(ControllerAction.UpdateShipment, {}, {} as MollieClient);
+    expect(mockedUpdateShipment).toBeCalledTimes(1);
+  });
+  it('should call CancelOrder if the action is CancelOrder', async () => {
+    const mockedCancelOrder = mocked(actions.cancelOrder);
+    await processAction(ControllerAction.CancelOrder, {}, {} as MollieClient);
+    expect(mockedCancelOrder).toBeCalledTimes(1);
+  });
+  it('should return an error if the action does not exist', async () => {
+    const expectedError = {
+      status: 400,
+      errors: [{ code: 'InvalidOperation', message: 'Error processing request, please check request and try again' }],
+    };
+    const processActionResult = await processAction('nonExistingActionString' as ControllerAction, {}, {} as MollieClient);
+    expect(processActionResult).toEqual(expectedError);
   });
 });
