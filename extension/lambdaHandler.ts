7b14deca3c55095422b2cb824ec9b2b8fc52e754
Richard Corke <richard.corke@and.digital>
2021-11-19 13:15:16 +0100

    update handler formatting

--- extension/lambdaHandler.ts
+++ extension/lambdaHandler.ts
@@ -1,19 +1,7 @@
 import ServerlessHttp from 'serverless-http';
 import app from './src/app';
 
-<<<<<<< HEAD
 const handler = ServerlessHttp(app);
 exports.handler = async (event: any, context: any) => {
   return await handler(event, context);
-=======
-const mollieClient = createMollieClient({ apiKey: config.mollie.apiKey });
-
-exports.handler = async () => {
-  // Methods for the Payments API
-  let methods = await mollieClient.methods.all();
-
-  // Methods for the Orders API
-  methods = await mollieClient.methods.all({ resource: 'orders' });
-  return methods;
->>>>>>> develop
 };
