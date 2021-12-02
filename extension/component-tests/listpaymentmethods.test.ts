4500d01b99ac1b3e9a0fb54f433ca4f02f7ca500
lebradley <lbradley@and.digital>
2021-12-02 09:53:59 +0100

    test: sets up component tests
    - uses nock to intercept and mock API responses
    - demo with listpaymentmethods

---
+++ extension/component-tests/listpaymentmethods.test.ts
@@ -0,0 +1,75 @@
+import nock from 'nock';
+import request from 'supertest';
+import app from '../src/app';
+import { paymentMethodsAvailableResponse, noPaymentMethodsAvailableResponse } from './mockResponses/mollieData/listpaymentmethods.data';
+
+describe('List Payment Methods', () => {
+  const mockCTPaymentObj = {
+    resource: {
+      obj: {
+        amountPlanned: {
+          currencyCode: 'EUR',
+          centAmount: 50000,
+        },
+        custom: {
+          fields: {
+            paymentMethodsRequest: '{}',
+          },
+        },
+      },
+    },
+  };
+  it('should return 200 and update action containing the count and methods available on mollie', async () => {
+    // Set up nock
+
+    const scope = nock('https://api.mollie.com/v2')
+      .get(/methods*/)
+      .reply(200, paymentMethodsAvailableResponse);
+
+    // Call API extension & trigger payment methods request
+    const res = await request(app).post('/').send(mockCTPaymentObj);
+
+    const { status, text } = res;
+    expect(status).toBe(200);
+
+    // Parse and check the update action
+    const parsedActions = JSON.parse(text);
+    const { actions } = parsedActions;
+    // expect(actions).toHaveLength(1); // currently fails - need to remove interfaceInteraction
+
+    const { name, value: stringifiedValue } = actions.find((action: any) => action.action === 'setCustomField');
+    expect(name).toBe('paymentMethodsResponse');
+
+    const value = JSON.parse(stringifiedValue);
+
+    expect(value.count).toBe(2);
+    expect(value.methods).toHaveLength(2);
+  });
+
+  it('should return 200 and update action containing "no methods found" if there are no available payment methods on mollie', async () => {
+    // Set up nock
+    const scope = nock('https://api.mollie.com/v2')
+      .get(/methods*/)
+      .reply(200, noPaymentMethodsAvailableResponse);
+
+    // Call API extension & trigger payment methods request
+    const res = await request(app).post('/').send(mockCTPaymentObj);
+
+    const { status, text } = res;
+    expect(status).toBe(200);
+
+    // Parse and check the update action
+    const parsedActions = JSON.parse(text);
+    const { actions } = parsedActions;
+    // expect(actions).toHaveLength(1); // currently fails - need to remove interfaceInteraction
+
+    const { name, value: stringifiedValue } = actions.find((action: any) => action.action === 'setCustomField');
+    expect(name).toBe('paymentMethodsResponse');
+
+    const value = JSON.parse(stringifiedValue);
+    expect(value).toEqual({
+      count: 0,
+      methods: 'NO_AVAILABLE_PAYMENT_METHODS',
+    });
+  });
+});
---
+++ extension/component-tests/mockResponses/mollieData/listpaymentmethods.data.ts
@@ -0,0 +1,123 @@
+export const paymentMethodsAvailableResponse = {
+  count: 2,
+  _embedded: {
+    methods: [
+      {
+        resource: 'method',
+        id: 'ideal',
+        description: 'iDEAL',
+        minimumAmount: {
+          value: '0.01',
+          currency: 'EUR',
+        },
+        maximumAmount: {
+          value: '50000.00',
+          currency: 'EUR',
+        },
+        image: {
+          size1x: 'https://mollie.com/external/icons/payment-methods/ideal.png',
+          size2x: 'https://mollie.com/external/icons/payment-methods/ideal%402x.png',
+          svg: 'https://mollie.com/external/icons/payment-methods/ideal.svg',
+        },
+        status: 'activated',
+        pricing: [
+          {
+            description: 'Netherlands',
+            fixed: {
+              value: '0.29',
+              currency: 'EUR',
+            },
+            variable: '0',
+          },
+        ],
+        _links: {
+          self: {
+            href: 'https://api.mollie.com/v2/methods/ideal',
+            type: 'application/hal+json',
+          },
+        },
+      },
+      {
+        resource: 'method',
+        id: 'creditcard',
+        description: 'Credit card',
+        minimumAmount: {
+          value: '0.01',
+          currency: 'EUR',
+        },
+        maximumAmount: {
+          value: '2000.00',
+          currency: 'EUR',
+        },
+        image: {
+          size1x: 'https://mollie.com/external/icons/payment-methods/creditcard.png',
+          size2x: 'https://mollie.com/external/icons/payment-methods/creditcard%402x.png',
+          svg: 'https://mollie.com/external/icons/payment-methods/creditcard.svg',
+        },
+        status: 'activated',
+        pricing: [
+          {
+            description: 'Commercial & non-European cards',
+            fixed: {
+              value: '0.25',
+              currency: 'EUR',
+            },
+            variable: '2.8',
+            feeRegion: 'other',
+          },
+          {
+            description: 'European cards',
+            fixed: {
+              value: '0.25',
+              currency: 'EUR',
+            },
+            variable: '1.8',
+            feeRegion: 'eu-cards',
+          },
+          {
+            description: 'American Express',
+            fixed: {
+              value: '0.25',
+              currency: 'EUR',
+            },
+            variable: '2.8',
+            feeRegion: 'amex',
+          },
+        ],
+        _links: {
+          self: {
+            href: 'https://api.mollie.com/v2/methods/creditcard',
+            type: 'application/hal+json',
+          },
+        },
+      },
+    ],
+  },
+  _links: {
+    self: {
+      href: 'https://api.mollie.com/v2/methods',
+      type: 'application/hal+json',
+    },
+    documentation: {
+      href: 'https://docs.mollie.com/reference/v2/methods-api/list-methods',
+      type: 'text/html',
+    },
+  },
+};
+
+export const noPaymentMethodsAvailableResponse = {
+  _embedded: {
+    methods: [],
+  },
+  count: 0,
+  _links: {
+    documentation: {
+      href: 'https://docs.mollie.com/reference/v2/methods-api/list-methods',
+      type: 'text/html',
+    },
+    self: {
+      href: 'https://api.mollie.com/v2/methods?amount%5Bvalue%5D=10&amount%5Bcurrency%5D=ISK&resource=orders',
+      type: 'application/hal+json',
+    },
+  },
+};
