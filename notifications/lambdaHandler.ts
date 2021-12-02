c086aa04f7eed72a06eabfbf8054d79a05f1d368
Richard Corke <richard.corke@and.digital>
2021-12-02 12:00:26 +0100

    feat/ refactor handle request for notifications, update lambda handler for notifications

--- notifications/lambdaHandler.ts
+++ notifications/lambdaHandler.ts
@@ -1,9 +1,37 @@
-import { Request, Response } from 'express'
-import { createMollieClient } from '@mollie/api-client';
 import config from './config/config';
+import { initialiseCommercetoolsClient } from './src/client/utils';
+import Logger from './src/logger/logger';
+import { handleOrderWebhook, handlePaymentWebhook } from './src/requestHandlers/handleRequest';
+import actions from './src/requestHandlers/index';
+import { AddTransaction, UpdateActionChangeTransactionState, UpdateActionSetCustomField } from './src/types/ctUpdateActions';
+import { isOrderOrPayment } from './src/utils';
 
-const mollieClient = createMollieClient({ apiKey: config.mollie.apiKey });
+exports.handler = async (event: any) => {
+  try {
+    // Reason for this check: if AWS API Gateway is used then event.body is provided as a string payload.
+    const body = event.body ? JSON.parse(event.body) : event;
+    const {
+      commercetools: { projectKey },
+    } = config;
+    const commercetoolsClient = initialiseCommercetoolsClient();
 
-exports.handler = async (req: Request, res: Response) => {
-  res.status(200).end()
+    const resourceType = isOrderOrPayment(body.id);
+
+    let updateActions: (UpdateActionChangeTransactionState | UpdateActionSetCustomField | AddTransaction)[] = [];
+    let mollieOrderId;
+    let ctPaymentVersion;
+
+    if (resourceType == 'order') {
+      handleOrderWebhook(body.id, ctPaymentVersion, updateActions);
+    } else {
+      handlePaymentWebhook(body.id, ctPaymentVersion, updateActions, mollieOrderId);
+    }
+
+    const ctKey = resourceType === 'order' ? body.id : mollieOrderId;
+    await actions.ctUpdatePaymentByKey(ctKey, commercetoolsClient, projectKey, ctPaymentVersion ?? 1, updateActions);
+    return;
+  } catch (error: any) {
+    Logger.error({ error });
+    return;
+  }
 };
