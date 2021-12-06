7d9699abf31ca28ec16c954d31b91d4d0bdca18e
denisand <90899394+denisand@users.noreply.github.com>
2021-12-06 15:18:29 +0100

    feat: moves paymentInterface check higher
    - groups tests into unhappy, happy path blocks in handleRequest
    - moves payment interface check before the determine action block

--- extension/tests/unit/requestHandlers/handleRequest.test.ts
+++ extension/tests/unit/requestHandlers/handleRequest.test.ts
@@ -22,6 +22,7 @@ describe('handleRequest', () => {
   const mockSend = jest.fn().mockReturnValue(res);
   const mockEnd = jest.fn();
   const mockLogError = jest.fn();
+  const mockLogDebug = jest.fn();
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -32,132 +33,140 @@ describe('handleRequest', () => {
     req.path = '/';
     req.method = 'POST';
     Logger.error = mockLogError;
+    Logger.debug = mockLogDebug;
   });
 
   afterAll(() => {
     jest.resetAllMocks();
   });
 
-  it('should return a list of actions and status 200 when processed action returns successfully', async () => {
-    mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.GetPaymentMethods, errorMessage: '' });
-    mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(true);
-    mocked(actions.getPaymentMethods).mockResolvedValue({ status: 200, actions: [{ action: 'update' }] });
+  describe('2xx - Happy Path', () => {
+    it('should return a list of actions and status 200 when processed action returns successfully', async () => {
+      mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(true);
+      mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.GetPaymentMethods, errorMessage: '' });
+      mocked(actions.getPaymentMethods).mockResolvedValueOnce({ status: 200, actions: [{ action: 'update' }] });
 
-    await handleRequest(req, res);
+      await handleRequest(req, res);
 
-    expect(mockStatus).toHaveBeenCalledWith(200);
-    expect(mockSend).toHaveBeenCalledWith({ actions: [{ action: 'update' }] });
-  });
-
-  it('should return status 200 if when the action is NoAction', async () => {
-    mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.NoAction, errorMessage: '' });
-
-    await handleRequest(req, res);
-
-    expect(mockStatus).toHaveBeenCalledWith(200);
-    expect(mockEnd).toHaveBeenCalledTimes(1);
-  });
-
-  it('should return status 200 if payment interface is not mollie', async () => {
-    mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.GetPaymentMethods, errorMessage: '' });
-    mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(false);
-
-    await handleRequest(req, res);
-
-    expect(mockStatus).toHaveBeenCalledWith(200);
-    expect(mockEnd).toHaveBeenCalledTimes(1);
-  });
-
-  it('should return status 400 if the request path is not /', async () => {
-    req.path = '/something';
-
-    await handleRequest(req, res);
-
-    expect(mockStatus).toHaveBeenCalledWith(400);
-    expect(mockEnd).toHaveBeenCalledTimes(1);
-  });
-
-  it('should return status 405 Method Not Allowed if the request method is not POST', async () => {
-    req.method = 'DELETE';
-
-    await handleRequest(req, res);
-
-    expect(mockStatus).toHaveBeenCalledWith(405);
-    expect(mockEnd).toHaveBeenCalledTimes(1);
-  });
-
-  it('should return 400 and the correct error message if the incoming payment object incorrectly tries to trigger an action', async () => {
-    mocked(determineAction).mockReturnValueOnce({
-      action: ControllerAction.NoAction,
-      errorMessage: 'Invalid paymentMethodInfo.method cash. Payment method must be set in order to make and manage payment transactions',
-    });
-    mocked(formatExtensionErrorResponse).mockReturnValueOnce({
-      status: 400,
-      errors: [
-        {
-          code: CTEnumErrors.InvalidInput,
-          message: 'Invalid paymentMethodInfo.method cash. Payment method must be set in order to make and manage payment transactions',
-        },
-      ],
+      expect(mockStatus).toHaveBeenLastCalledWith(200);
+      expect(mockSend).toHaveBeenCalledWith({ actions: [{ action: 'update' }] });
     });
 
-    await handleRequest(req, res);
+    it('should return status 200 if when the action is NoAction', async () => {
+      mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(true);
+      mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.NoAction, errorMessage: '' });
 
-    expect(mockStatus).toHaveBeenLastCalledWith(400);
-    expect(mockSend).toHaveBeenLastCalledWith({
-      errors: [
-        {
-          code: 'InvalidInput',
-          message: 'Invalid paymentMethodInfo.method cash. Payment method must be set in order to make and manage payment transactions',
-        },
-      ],
+      await handleRequest(req, res);
+
+      expect(mockStatus).toHaveBeenLastCalledWith(200);
+      expect(mockEnd).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return status 200 if payment interface is not mollie', async () => {
+      mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(false);
+
+      await handleRequest(req, res);
+
+      expect(mockStatus).toHaveBeenLastCalledWith(200);
+      expect(mockEnd).toHaveBeenCalledTimes(1);
     });
   });
 
-  it('should return status 400 and an array of formatted errors if an error happens whilst processing actions', async () => {
-    mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.GetPaymentMethods, errorMessage: '' });
-    mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(true);
-    mocked(actions.getPaymentMethods).mockResolvedValue({
-      status: 400,
-      errors: [
-        {
-          code: CTEnumErrors.Unauthorized,
-          message: 'API Key error',
-        },
-      ],
+  describe('4xx - Unhappy Path - API extension called incorrectly', () => {
+    it('should return status 400 if the request path is not /', async () => {
+      req.path = '/something';
+
+      await handleRequest(req, res);
+
+      expect(mockStatus).toHaveBeenLastCalledWith(400);
+      expect(mockEnd).toHaveBeenCalledTimes(1);
     });
 
-    await handleRequest(req, res);
+    it('should return status 405 Method Not Allowed if the request method is not POST', async () => {
+      req.method = 'DELETE';
 
-    expect(mockStatus).toHaveBeenCalledWith(400);
-    expect(mockSend).toHaveBeenCalledWith({
-      errors: [
-        {
-          code: CTEnumErrors.Unauthorized,
-          message: 'API Key error',
-        },
-      ],
+      await handleRequest(req, res);
+
+      expect(mockStatus).toHaveBeenLastCalledWith(405);
+      expect(mockEnd).toHaveBeenCalledTimes(1);
     });
   });
 
-  it('should catch and handle errors and return a general error response to CT', async () => {
-    const mockError = new Error('Something went wrong');
-    mockError.name = 'Big error';
-    mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.GetPaymentMethods, errorMessage: '' });
-    mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(true);
-    mocked(actions.getPaymentMethods).mockRejectedValue({ name: 'Big error', message: 'Something went wrong' });
+  describe('4xx - Unhappy Path - Error processing action', () => {
+    it('should return 400 and the correct error message if the incoming payment object incorrectly tries to trigger an action', async () => {
+      mocked(determineAction).mockReturnValueOnce({
+        action: ControllerAction.NoAction,
+        errorMessage: 'Invalid paymentMethodInfo.method cash. Payment method must be set in order to make and manage payment transactions',
+      });
+      mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(true);
+      mocked(formatExtensionErrorResponse).mockReturnValueOnce({
+        status: 400,
+        errors: [
+          {
+            code: CTEnumErrors.InvalidInput,
+            message: 'Invalid paymentMethodInfo.method cash. Payment method must be set in order to make and manage payment transactions',
+          },
+        ],
+      });
 
-    await handleRequest(req, res);
-    expect(mockStatus).toHaveBeenCalledWith(400);
-    expect(mockSend).toHaveBeenCalledWith({
-      errors: [
-        {
-          code: CTEnumErrors.General,
-          message: 'error_name: Big error, error_message: Something went wrong',
-        },
-      ],
+      await handleRequest(req, res);
+
+      // expect(mockStatus).toHaveBeenLastCalledWith(400);
+      expect(mockSend).toHaveBeenLastCalledWith({
+        errors: [
+          {
+            code: 'InvalidInput',
+            message: 'Invalid paymentMethodInfo.method cash. Payment method must be set in order to make and manage payment transactions',
+          },
+        ],
+      });
     });
-    expect(mockLogError).toHaveBeenCalledTimes(1);
+
+    it('should return status 400 and an array of formatted errors if an error happens whilst processing actions', async () => {
+      mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(true);
+      mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.GetPaymentMethods, errorMessage: '' });
+      mocked(actions.getPaymentMethods).mockResolvedValue({
+        status: 400,
+        errors: [
+          {
+            code: CTEnumErrors.Unauthorized,
+            message: 'API Key error',
+          },
+        ],
+      });
+
+      await handleRequest(req, res);
+
+      expect(mockStatus).toHaveBeenLastCalledWith(400);
+      expect(mockSend).toHaveBeenLastCalledWith({
+        errors: [
+          {
+            code: CTEnumErrors.Unauthorized,
+            message: 'API Key error',
+          },
+        ],
+      });
+    });
+
+    it('should catch and handle errors and return a general error response to CT', async () => {
+      const mockError = new Error('Something went wrong');
+      mockError.name = 'Big error';
+      mocked(ut.isMolliePaymentInterface).mockReturnValueOnce(true);
+      mocked(determineAction).mockReturnValueOnce({ action: ControllerAction.GetPaymentMethods, errorMessage: '' });
+      mocked(actions.getPaymentMethods).mockRejectedValue({ name: 'Big error', message: 'Something went wrong' });
+
+      await handleRequest(req, res);
+      expect(mockStatus).toHaveBeenLastCalledWith(400);
+      expect(mockSend).toHaveBeenLastCalledWith({
+        errors: [
+          {
+            code: CTEnumErrors.General,
+            message: 'error_name: Big error, error_message: Something went wrong',
+          },
+        ],
+      });
+      expect(mockLogError).toHaveBeenCalledTimes(1);
+    });
   });
 });
 
