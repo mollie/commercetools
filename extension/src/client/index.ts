f2ed94124fe7c9a959ce216f521c4bb7d519f15f
Denis Androic <denis.androic@and.digital>
2021-12-08 10:41:18 +0100

    refactor: split client creation

--- extension/src/client/index.ts
+++ extension/src/client/index.ts
@@ -1,46 +1,4 @@
-import fetch from 'node-fetch-commonjs';
-import createMollieClient, { MollieClient } from '@mollie/api-client';
-import { createAuthMiddlewareForClientCredentialsFlow } from '@commercetools/sdk-middleware-auth';
-import { createHttpMiddleware } from '@commercetools/sdk-middleware-http';
-import { createUserAgentMiddleware } from '@commercetools/sdk-middleware-user-agent';
-import { createClient } from '@commercetools/sdk-client';
+import initialiseCommercetoolsClient from './initialiseCommercetoolsClient'
+import initialiseMollieClient from './initialiseMollieClient'
 
-import config from '../../config/config';
-import { version } from '../../package.json';
-
-export function initialiseMollieClient(): MollieClient {
-  const mollieApiKey = config.mollie.apiKey;
-  const mollieUserAgentString = `MollieCommercetools-extension/${version}`;
-  const mollieClient = createMollieClient({ apiKey: mollieApiKey, versionStrings: mollieUserAgentString });
-  return mollieClient;
-}
-
-export function initialiseCommercetoolsClient(): any {
-  const {
-    commercetools: { projectKey, clientId, clientSecret, host, authUrl, scopes },
-  } = config;
-
-  const userAgentMiddleware = createUserAgentMiddleware({
-    libraryName: 'MollieCommercetools-notification',
-    libraryVersion: version,
-  });
-
-  const ctAuthMiddleware = createAuthMiddlewareForClientCredentialsFlow({
-    host: authUrl,
-    projectKey,
-    credentials: {
-      clientId,
-      clientSecret,
-    },
-    scopes,
-    fetch,
-  });
-
-  const ctHttpMiddleWare = createHttpMiddleware({
-    host,
-    fetch,
-  });
-
-  const commercetoolsApi = createClient({ middlewares: [userAgentMiddleware, ctAuthMiddleware, ctHttpMiddleWare] });
-  return { commercetoolsApi, projectKey };
-}
+export { initialiseCommercetoolsClient, initialiseMollieClient }
