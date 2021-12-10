7700d007445c8f38f21e548222fc0bc2a46b2075
lebradley <lbradley@and.digital>
2021-12-10 16:42:34 +0100

    [WIP]feat: adds addTransaction method

--- notifications/src/makeActions.ts
+++ notifications/src/makeActions.ts
@@ -1,4 +1,20 @@
-import { CTTransactionState } from './types/ctPaymentTypes';
+import { CTTransactionState, CTTransactionType } from './types/ctPaymentTypes';
+import { AddTransaction, UpdateActionChangeTransactionState, UpdateActionKey } from './types/ctUpdateActions';
+import { convertMollieAmountToCTMoney } from './utils';
+
+const addTransaction = (type: CTTransactionType, amount: { currency: string; value: string }, interactionId: string, state: CTTransactionState, timestamp: string): AddTransaction => {
+  const ctAmount = convertMollieAmountToCTMoney(amount);
+  return {
+    action: UpdateActionKey.AddTransaction,
+    transaction: {
+      type,
+      amount: ctAmount,
+      timestamp,
+      interactionId,
+      state,
+    },
+  };
+};
 
 const setStatusInterfaceText = (interfaceText: string) => {
   return {
@@ -7,14 +23,15 @@ const setStatusInterfaceText = (interfaceText: string) => {
   };
 };
 
-const changeTransactionState = (id: string, newState: CTTransactionState) => {
+const changeTransactionState = (id: string, newState: CTTransactionState): UpdateActionChangeTransactionState => {
   return {
-    action: 'changeTransactionState',
+    action: UpdateActionKey.ChangeTransactionState,
     transactionId: id,
     state: newState,
   };
 };
 export const makeActions = {
+  addTransaction,
   changeTransactionState,
   setStatusInterfaceText,
 };
