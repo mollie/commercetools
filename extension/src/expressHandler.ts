9ad1f1b3582462b185993f3270e00b31436c6182
Higor Ramos <higor.ramos@and.digital>
2021-12-13 16:27:37 +0100

    fix one test

--- extension/src/expressHandler.ts
+++ extension/src/expressHandler.ts
@@ -8,7 +8,7 @@ export default async function handler(req: Request, res: Response) {
     const requestInput = new HandleRequestInput(req.path, req.method, req.body);
     const result = await handleRequest(requestInput);
     if (result instanceof HandleRequestSuccess) {
-        return res.status(200).send({actions: actions});
+        return res.status(200).send({actions: result.actions});
 
     } else {
         return res.status(result.status).send({errors: result.errors});
