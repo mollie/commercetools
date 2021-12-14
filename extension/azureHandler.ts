6237614c13234134bf0d8c99f83f58136e1a6bcb
Higor Ramos <higor.ramos@and.digital>
2021-12-14 15:07:23 +0100

    azure deploy config + small docs changes

--- extension/azureHandler.ts
+++ extension/azureHandler.ts
@@ -1,14 +1,16 @@
 import {AzureFunction, Context, HttpRequest} from "@azure/functions"
 import {HandleRequestInput, HandleRequestSuccess} from "./src/types";
-import handleRequest from "./src/requestHandlers/handleRequest";
 
+loadSettings();
+
+import handleRequest from "./src/requestHandlers/handleRequest";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
     /*
         The azure HttpRequest object does not expose the path, it can be configured directly in the function config
         https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook-trigger?tabs=javascript
      */
-    const requestInput = new HandleRequestInput('/', req.method, req.body);
+    const requestInput = new HandleRequestInput('/', req.method!.toString(), req.body);
     const result = await handleRequest(requestInput);
     if (result instanceof HandleRequestSuccess) {
         context.res = {
@@ -22,5 +24,31 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         }
     }
 };
+
+/*
+    Azure settings do not support nested json configuration, so it needs manual object creation
+    https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-node?tabs=v2#access-environment-variables-in-code
+ */
+function loadSettings(){
+    const config = {
+        commercetools: {
+            authUrl: process.env["CT_MOLLIE_CONFIG:commercetools:authUrl"],
+            clientId: process.env["CT_MOLLIE_CONFIG:commercetools:clientId"],
+            clientSecret: process.env["CT_MOLLIE_CONFIG:commercetools:clientSecret"],
+            host: process.env["CT_MOLLIE_CONFIG:commercetools:host"],
+            projectKey: process.env["CT_MOLLIE_CONFIG:commercetools:projectKey"]
+        },
+        mollie: {
+            apiKey: process.env["CT_MOLLIE_CONFIG:mollie:apiKey"]
+        },
+        service: {
+            port: process.env["CT_MOLLIE_CONFIG:service:port"],
+            logLevel: process.env["CT_MOLLIE_CONFIG:service:logLevel"],
+            logTransports: process.env["CT_MOLLIE_CONFIG:service:logTransports"],
+        }
+
+    }
+    process.env.CT_MOLLIE_CONFIG = JSON.stringify(config);
+}
 
 export default httpTrigger;
