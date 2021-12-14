e7a6733cec3c20f28fb3d8096f6bde449b0429bd
Lindsay B <lbradley@and.digital>
2021-12-14 14:39:01 +0100

    Feat/cmi 134 update notifications actions (#58)
    * feat: adds makeActions factory for updateActions
    
    * refactor: uses makeActions
    
    * [WIP]feat: adds addTransaction method
    
    * chore: remove todos

---
+++ notifications/tests/makeActions.test.ts
@@ -0,0 +1,46 @@
+import { makeActions } from '../src/makeActions';
+import { CTTransactionState, CTTransactionType } from '../src/types/ctPaymentTypes';
+
+describe('makeActions', () => {
+  it('addTransaction - should return update action with new transaction to be added', () => {
+    const mockMollieAmount = {
+      currency: 'EUR',
+      value: '10.00',
+    };
+    const timestamp = '2021-08-02T09:29:56+00:00';
+    const updateAction = makeActions.addTransaction(CTTransactionType.Authorization, mockMollieAmount, '546ad86a-f97a-4b82-938a-09368bb50217', CTTransactionState.Failure, timestamp);
+    expect(updateAction).toEqual({
+      action: 'addTransaction',
+      transaction: {
+        type: 'Authorization',
+        amount: {
+          currencyCode: 'EUR',
+          centAmount: 1000,
+          fractionDigits: 2,
+          type: 'centPrecision',
+        },
+        interactionId: '546ad86a-f97a-4b82-938a-09368bb50217',
+        state: 'Failure',
+        timestamp,
+      },
+    });
+  });
+
+  it('changeTransactionState - should return update action with new state', () => {
+    const mockTransactionId = '04e225cf-7de7-4fd7-a9e7-258ec9e32d3e';
+    const updateAction = makeActions.changeTransactionState(mockTransactionId, CTTransactionState.Success);
+    expect(updateAction).toEqual({
+      action: 'changeTransactionState',
+      transactionId: mockTransactionId,
+      state: CTTransactionState.Success,
+    });
+  });
+
+  it('setStatusInterfaceText - should return update action with a mollie order status text', () => {
+    const updateAction = makeActions.setStatusInterfaceText('paid');
+    expect(updateAction).toEqual({
+      action: 'setMethodInfoName',
+      interfaceText: 'paid',
+    });
+  });
+});
