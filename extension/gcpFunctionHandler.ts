18d305064343a31805c85dac8aba9c38f87bbe63
denisand <90899394+denisand@users.noreply.github.com>
2021-12-22 15:42:53 +0100

    fix: return actionResult status on handlers (#78)
    * fix: return actionResult status on handlers
    * fix: cloud builder script
    * style: fix linting on cloud handlers

--- extension/gcpFunctionHandler.ts
+++ extension/gcpFunctionHandler.ts
@@ -1,21 +1,21 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import handleRequest from './src/requestHandlers/handleRequest';
-import {HandleRequestInput, HandleRequestSuccess} from "./src/types";
+import { HandleRequestInput, HandleRequestSuccess } from './src/types';
 
 exports.handler = async (req: Request, res: Response) => {
-    const requestInput = new HandleRequestInput(req.path, req.method, req.body);
-    const result = await handleRequest(requestInput);
-    if (result instanceof HandleRequestSuccess) {
-        if (result.actions && result.actions.length > 0) {
-            return res.status(200).send({actions: result.actions});
-        } else {
-            return res.status(200).end();
-        }
+  const requestInput = new HandleRequestInput(req.path, req.method, req.body);
+  const result = await handleRequest(requestInput);
+  if (result instanceof HandleRequestSuccess) {
+    if (result.actions && result.actions.length > 0) {
+      return res.status(result.status).send({ actions: result.actions });
     } else {
-        if (result.errors && result.errors.length > 0) {
-            return res.status(result.status).send({errors: result.errors});
-        } else {
-            return res.status(result.status).end();
-        }
+      return res.status(result.status).end();
     }
+  } else {
+    if (result.errors && result.errors.length > 0) {
+      return res.status(result.status).send({ errors: result.errors });
+    } else {
+      return res.status(result.status).end();
+    }
+  }
 };
