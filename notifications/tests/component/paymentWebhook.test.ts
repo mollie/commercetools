bece702bcd1bdb2de6d67fa14acbebf83840bfb7
denisand <90899394+denisand@users.noreply.github.com>
2022-01-19 14:43:14 +0000

    Feat/cmi 140 notification tests (#120)
    * test: add 'add transaction' test
    * test: add refund case to the payment webhook test, clean up sample data
    * fix: refund status and nock path check
    * chore: split up refund cases

--- notifications/tests/component/paymentWebhook.test.ts
+++ notifications/tests/component/paymentWebhook.test.ts
@@ -1,5 +1,6 @@
 import nock from 'nock';
 import request from 'supertest';
+import { omit } from 'lodash';
 import app from '../../src/app';
 import config from '../../config/config';
 import { mockPaidPayment } from './mockResponses/mollieData/payment.data';
@@ -32,6 +33,7 @@ describe('Webhook triggered with Mollie payment ID as payload', () => {
 
   it('should update the commercetools transaction state when transaction state is different than Mollie payment status', async () => {
     const mockBody = { id: 'tr_ncaPcAhuUV' };
+    const mockPaidPaymentNoRefund = omit(mockPaidPayment, '_embedded');
     const expectedUpdateBody = {
       version: 25,
       actions: [
@@ -39,6 +41,111 @@ describe('Webhook triggered with Mollie payment ID as payload', () => {
           action: 'changeTransactionState',
           transactionId: '2020335e-1ea2-4d49-b45b-14a078f589a6',
           state: 'Success',
+        },
+      ],
+    };
+    const molliePaymentScope = nock('https://api.mollie.com/v2')
+      .get(uri => uri.includes('payments/tr_ncaPcAhuUV'))
+      .reply(200, mockPaidPaymentNoRefund);
+    const ctGetPaymentScope = nock(`${host}/${projectKey}`)
+      .get(uri => uri.includes('key=ord_12345'))
+      .reply(200, ctPaymentResponse);
+    const ctUpdatePaymentScope = nock(`${host}/${projectKey}`)
+      .post(uri => uri.includes('key=ord_12345'), expectedUpdateBody)
+      .reply(200, {});
+
+    const { status } = await request(app).post('/').send(mockBody);
+    expect(status).toBe(200);
+    expect(molliePaymentScope.isDone()).toBe(true);
+    expect(ctGetPaymentScope.isDone()).toBe(true);
+    expect(ctUpdatePaymentScope.isDone()).toBe(true);
+  });
+  it('should add the commercetools transaction when none of existing transactions correspond to Mollie payment', async () => {
+    const newId = 'tr_3nPcU3Epcj';
+    const mockBody = { id: newId };
+    const mockPaidPaymentNoRefund = omit(mockPaidPayment, '_embedded');
+    mockPaidPaymentNoRefund.id = newId;
+    const expectedUpdateBody = {
+      version: 25,
+      actions: [
+        {
+          action: 'addTransaction',
+          transaction: {
+            amount: {
+              type: 'centPrecision',
+              currencyCode: 'EUR',
+              centAmount: 1000,
+              fractionDigits: 2,
+            },
+            state: 'Success',
+            type: 'Charge',
+            interactionId: newId,
+          },
+        },
+      ],
+    };
+    const molliePaymentScope = nock('https://api.mollie.com/v2')
+      .get(uri => uri.includes(`payments/${newId}?embed=refunds`))
+      .reply(200, mockPaidPaymentNoRefund);
+    const ctGetPaymentScope = nock(`${host}/${projectKey}`)
+      .get(uri => uri.includes('key=ord_12345'))
+      .reply(200, ctPaymentResponse);
+    const ctUpdatePaymentScope = nock(`${host}/${projectKey}`)
+      .post(uri => uri.includes('key=ord_12345'), expectedUpdateBody)
+      .reply(200, {});
+
+    const { status } = await request(app).post('/').send(mockBody);
+    expect(status).toBe(200);
+    expect(molliePaymentScope.isDone()).toBe(true);
+    expect(ctGetPaymentScope.isDone()).toBe(true);
+    expect(ctUpdatePaymentScope.isDone()).toBe(true);
+  });
+  it('should update refund status', async () => {
+    const mockBody = { id: 'tr_ncaPcAhuUV' };
+    ctPaymentResponse.transactions = [
+      {
+        id: '2020335e-1ea2-4d49-b45b-14a078f589a6',
+        timestamp: '2022-01-03T15:49:11.000Z',
+        type: 'Authorization',
+        amount: {
+          type: 'centPrecision',
+          currencyCode: 'EUR',
+          centAmount: 1000,
+          fractionDigits: 2,
+        },
+        interactionId: 'tr_ncaPcAhuUV',
+        state: 'Success',
+      },
+      {
+        id: '2020335e-1ea2-4d49-b45b-14a078f589a6',
+        timestamp: '2022-01-03T15:49:11.000Z',
+        type: 'Refund',
+        amount: {
+          type: 'centPrecision',
+          currencyCode: 'EUR',
+          centAmount: 1000,
+          fractionDigits: 2,
+        },
+        interactionId: 'tr_ncaPcAhuUV',
+        state: 'Initial',
+      },
+    ];
+    const expectedUpdateBody = {
+      version: 25,
+      actions: [
+        {
+          action: 'addTransaction',
+          transaction: {
+            type: 'Refund',
+            amount: {
+              type: 'centPrecision',
+              currencyCode: 'EUR',
+              centAmount: 142,
+              fractionDigits: 2,
+            },
+            interactionId: 're_8eP2Kzt9G9',
+            state: 'Success',
+          },
         },
       ],
     };
