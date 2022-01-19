71f83098aa61afbbbcf0f384b5e9a56ef42f6543
Denis Androic <denis.androic@and.digital>
2022-01-19 13:11:30 +0000

    chore: split up refund cases

--- notifications/tests/component/paymentWebhook.test.ts
+++ notifications/tests/component/paymentWebhook.test.ts
@@ -33,6 +33,7 @@ describe('Webhook triggered with Mollie payment ID as payload', () => {
 
   it('should update the commercetools transaction state when transaction state is different than Mollie payment status and update refund status', async () => {
     const mockBody = { id: 'tr_ncaPcAhuUV' };
+    const mockPaidPaymentNoRefund = omit(mockPaidPayment, '_embedded');
     const expectedUpdateBody = {
       version: 25,
       actions: [
@@ -41,25 +42,11 @@ describe('Webhook triggered with Mollie payment ID as payload', () => {
           transactionId: '2020335e-1ea2-4d49-b45b-14a078f589a6',
           state: 'Success',
         },
-        {
-          action: 'addTransaction',
-          transaction: {
-            type: 'Refund',
-            amount: {
-              type: 'centPrecision',
-              currencyCode: 'EUR',
-              centAmount: 142,
-              fractionDigits: 2,
-            },
-            interactionId: 're_8eP2Kzt9G9',
-            state: 'Success',
-          },
-        },
       ],
     };
     const molliePaymentScope = nock('https://api.mollie.com/v2')
       .get(uri => uri.includes('payments/tr_ncaPcAhuUV'))
-      .reply(200, mockPaidPayment);
+      .reply(200, mockPaidPaymentNoRefund);
     const ctGetPaymentScope = nock(`${host}/${projectKey}`)
       .get(uri => uri.includes('key=ord_12345'))
       .reply(200, ctPaymentResponse);
@@ -100,6 +87,71 @@ describe('Webhook triggered with Mollie payment ID as payload', () => {
     const molliePaymentScope = nock('https://api.mollie.com/v2')
       .get(uri => uri.includes(`payments/${newId}?embed=refunds`))
       .reply(200, mockPaidPaymentNoRefund);
+    const ctGetPaymentScope = nock(`${host}/${projectKey}`)
+      .get(uri => uri.includes('key=ord_12345'))
+      .reply(200, ctPaymentResponse);
+    const ctUpdatePaymentScope = nock(`${host}/${projectKey}`)
+      .post(uri => uri.includes('key=ord_12345'), expectedUpdateBody)
+      .reply(200, {});
+
+    const { status } = await request(app).post('/').send(mockBody);
+    expect(status).toBe(200);
+    expect(molliePaymentScope.isDone()).toBe(true);
+    expect(ctGetPaymentScope.isDone()).toBe(true);
+    expect(ctUpdatePaymentScope.isDone()).toBe(true);
+  });
+  it('should update refund status', async () => {
+    const mockBody = { id: 'tr_ncaPcAhuUV' };
+    ctPaymentResponse.transactions = [
+      {
+        id: '2020335e-1ea2-4d49-b45b-14a078f589a6',
+        timestamp: '2022-01-03T15:49:11.000Z',
+        type: 'Authorization',
+        amount: {
+          type: 'centPrecision',
+          currencyCode: 'EUR',
+          centAmount: 1000,
+          fractionDigits: 2,
+        },
+        interactionId: 'tr_ncaPcAhuUV',
+        state: 'Success',
+      },
+      {
+        id: '2020335e-1ea2-4d49-b45b-14a078f589a6',
+        timestamp: '2022-01-03T15:49:11.000Z',
+        type: 'Refund',
+        amount: {
+          type: 'centPrecision',
+          currencyCode: 'EUR',
+          centAmount: 1000,
+          fractionDigits: 2,
+        },
+        interactionId: 'tr_ncaPcAhuUV',
+        state: 'Initial',
+      },
+    ];
+    const expectedUpdateBody = {
+      version: 25,
+      actions: [
+        {
+          action: 'addTransaction',
+          transaction: {
+            type: 'Refund',
+            amount: {
+              type: 'centPrecision',
+              currencyCode: 'EUR',
+              centAmount: 142,
+              fractionDigits: 2,
+            },
+            interactionId: 're_8eP2Kzt9G9',
+            state: 'Success',
+          },
+        },
+      ],
+    };
+    const molliePaymentScope = nock('https://api.mollie.com/v2')
+      .get(uri => uri.includes('payments/tr_ncaPcAhuUV'))
+      .reply(200, mockPaidPayment);
     const ctGetPaymentScope = nock(`${host}/${projectKey}`)
       .get(uri => uri.includes('key=ord_12345'))
       .reply(200, ctPaymentResponse);
