db4c2572342f476c62e8b3be44448f8f399646c1
Denis Androic <denis.androic@and.digital>
2022-04-21 13:19:40 +0200

    fix: azure handler webhook request

--- notifications/azureHandler.ts
+++ notifications/azureHandler.ts
@@ -1,5 +1,6 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { HandleRequestInput, HandleRequestSuccess } from './src/types/requestHandler';
+import querystring from 'querystring';
 
 loadSettings();
 
@@ -10,7 +11,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         The azure HttpRequest object does not expose the path, it can be configured directly in the function config
         https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook-trigger?tabs=javascript
      */
-  const requestInput = new HandleRequestInput('/', req.method!.toString(), req.body);
+  const requestInput = new HandleRequestInput('/', req.method!.toString(), querystring.parse(req.body));
   const result = await handleRequest(requestInput);
   if (result instanceof HandleRequestSuccess) {
     context.res = {
