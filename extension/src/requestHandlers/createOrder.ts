3ba3fe98d387c6ff6fc61faeec4596bead6fd293
Fifciuu <fjedrasik@vuestorefront.io>
2022-05-09 12:42:24 +0200

    fix

--- extension/src/requestHandlers/createOrder.ts
+++ extension/src/requestHandlers/createOrder.ts
@@ -277,7 +277,7 @@ export default async function createOrder(
     Logger.debug('orderParams: %o', orderParams);
     const mollieCreatedOrder = await mollieClient.orders.create(orderParams);
     Logger.debug('mollieCreatedOrder: %o', mollieCreatedOrder);
-    const ctActions = await createCtActions(mollieCreatedOrder, ctPayment, cartByPayment.id);
+    const ctActions = await createCtActions(mollieCreatedOrder, ctPayment, cart.id);
     return {
       actions: ctActions,
       status: 201,
