9f777e4ffce2f0812d2300fc3901786a8d855c52
Denis Androic <denis.androic@and.digital>
2023-01-05 17:17:39 +0100

    chore: update critical packages, fix tests, update error check

--- notifications/tests/unit/requestHandlers/mollie/getPaymentDetailsById.test.ts
+++ notifications/tests/unit/requestHandlers/mollie/getPaymentDetailsById.test.ts
@@ -1,6 +1,5 @@
 import { MollieClient } from '@mollie/api-client';
-import PaymentsResource from '@mollie/api-client/dist/types/src/resources/payments/PaymentsResource';
-import { mocked } from 'ts-jest/utils';
+import PaymentsBinder from '@mollie/api-client/dist/types/src/binders/payments/PaymentsBinder';
 import getPaymentDetailsById from '../../../../src/requestHandlers/mollie/getPaymentDetailsById';
 import Logger from '../../../../src/logger/logger';
 
@@ -9,20 +8,20 @@ jest.mock('@mollie/api-client');
 describe('getOrderDetailsById', () => {
   const mockLogDebug = jest.fn();
   const mockMollieClient = {} as MollieClient;
-  const mockPaymentsResource = {} as PaymentsResource;
+  const mockPaymentsBinder = {} as PaymentsBinder;
 
   // payments.get() on Mollie Client has two overloads
   // One returns Payment type, one returns never
   // To get typescript to compile, had to cast the mock response as 'never' (any does not work)
   const mockPayment = { id: 'tr_12345' } as never;
 
-  mockMollieClient.payments = mockPaymentsResource;
+  mockMollieClient.payments = mockPaymentsBinder;
 
   beforeEach(() => {
     jest.clearAllMocks();
     const mockGetPayment = jest.fn().mockImplementationOnce(() => mockPayment);
-    mockPaymentsResource.get = mockGetPayment;
-    mocked(mockPaymentsResource.get).mockResolvedValueOnce(mockPayment);
+    mockPaymentsBinder.get = mockGetPayment;
+    jest.mocked(mockPaymentsBinder.get).mockResolvedValueOnce(mockPayment);
     Logger.debug = mockLogDebug;
   });
 
@@ -36,15 +35,15 @@ describe('getOrderDetailsById', () => {
   });
 
   it('should format 404 errors and add source information', async () => {
-    const orderNotFoundError = jest.fn().mockRejectedValue({ message: 'Payment not found', status: 404 });
-    mockPaymentsResource.get = orderNotFoundError;
+    const orderNotFoundError = jest.fn().mockRejectedValue({ message: 'Payment not found', statusCode: 404 });
+    mockPaymentsBinder.get = orderNotFoundError;
 
     await expect(getPaymentDetailsById('tr_12345', mockMollieClient)).rejects.toEqual({ message: 'Payment not found', source: 'mollie', status: 404 });
   });
 
   it('should log full error (at debug level) then throw the error if mollie call fails', async () => {
     const getPaymentFailure = jest.fn().mockRejectedValue(new Error('Mollie Error'));
-    mockPaymentsResource.get = getPaymentFailure;
+    mockPaymentsBinder.get = getPaymentFailure;
 
     await expect(getPaymentDetailsById('tr_12345', mockMollieClient)).rejects.toThrow(Error);
     expect(mockLogDebug).toHaveBeenCalledTimes(1);
