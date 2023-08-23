efe063f9329c286724708c73bb5a7dfc6906faab
Paolo Brunasti <119290234+brunastiatand@users.noreply.github.com>
2023-08-23 13:28:34 +0200

    Restructures the response format to CommerceTools Extension

--- extension/lambdaHandler.ts
+++ extension/lambdaHandler.ts
@@ -1,28 +1,24 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { createCorrelationId } from './src/utils';
 import handleRequest from './src/requestHandlers/handleRequest';
-import { HandleRequestInput, HandleRequestSuccess } from './src/types';
+import { HandleRequestInput, HandleRequestSuccess, Action } from './src/types';
 
 exports.handler = async (event: APIGatewayProxyEvent) => {
   const body = event.body ? JSON.parse(event.body) : event;
 
-  const headers = new Map([['authorization', event.headers['authorization'] ?? '']]);
+  const headers = new Map([['authorization', event.headers?.['authorization'] ?? '']]);
   const input = new HandleRequestInput(event.path, event.httpMethod, body, headers);
 
   let result = await handleRequest(input);
 
   if (result instanceof HandleRequestSuccess) {
     return {
-      headers: { ...event.headers, 'x-correlation-id': event.headers['x-correlation-id'] ?? createCorrelationId() },
-      statusCode: result.status,
-      body: JSON.stringify({
         responseType: 'UpdateRequest',
-        actions: result.actions,
-      }),
+        actions: result.actions
     };
   } else {
     return {
-      headers: { ...event.headers, 'x-correlation-id': event.headers['x-correlation-id'] ?? createCorrelationId() },
+      headers: { ...event.headers, 'x-correlation-id': event.headers?.['x-correlation-id'] ?? createCorrelationId() },
       statusCode: result.status,
       body: JSON.stringify({
         responseType: 'FailedValidation',
