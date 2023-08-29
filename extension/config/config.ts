3c00d4bf3259d18c29d07c04b2ad56b2256b7fdb
Paolo Brunasti <119290234+brunastiatand@users.noreply.github.com>
2023-08-29 12:13:11 +0200

    Fixed broken changes
    Previously edited the wrong file

--- extension/config/config.ts
+++ extension/config/config.ts
@@ -7,9 +7,6 @@ const isConfigValid = (config: Config): { valid: boolean; message: string } => {
   if (!mollie?.apiKey) {
     message = message + 'No Mollie API Key found\n';
   }
-  if (!config.commercetools) {
-    message = message + 'No Commercetools configuration present\n';
-  }
   if (!commercetools?.host || !commercetools?.clientId || !commercetools?.clientSecret || !commercetools?.authUrl || !commercetools?.projectKey) {
     message = message + 'Commercetools configuration requires missing required key(s)\n';
   }
@@ -26,7 +23,7 @@ export function loadConfig(ctMollieConfig: string | undefined) {
     const config: Config = {
       ...envConfig,
       service: {
-        port: envConfig.service?.port || 3001,
+        port: envConfig.service?.port || 3000,
         logLevel: process.env.LOG_LEVEL || envConfig.service?.logLevel || 'info',
         logTransports: envConfig.service?.logTransports || 'terminal',
       },
@@ -34,7 +31,13 @@ export function loadConfig(ctMollieConfig: string | undefined) {
 
     const localeRegex = /^[a-z]{2}_[A-Z]{2}$/;
 
-    Object.assign(config.service, envConfig.service?.locale && envConfig.service.locale.match(localeRegex) && { locale: envConfig.service.locale });
+    Object.assign(
+      config.service,
+      envConfig.service?.webhookUrl && { webhookUrl: envConfig.service.webhookUrl },
+      envConfig.service?.redirectUrl && { redirectUrl: envConfig.service.redirectUrl },
+      envConfig.service?.locale && envConfig.service.locale.match(localeRegex) && { locale: envConfig.service.locale },
+    );
+    Object.assign(config.commercetools, envConfig.commercetools?.authentication ? { authentication: envConfig.commercetools.authentication } : { authentication: { isBasicAuth: false } });
     Object.assign(config.commercetools, { enableRetry: envConfig.commercetools.enableRetry ?? true });
 
     const { valid, message } = isConfigValid(config);
