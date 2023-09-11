e39aa918a6f26841821e1f88389eaa270f6b345d
Paolo Brunasti <119290234+brunastiatand@users.noreply.github.com>
2023-09-11 09:55:42 +0200

    Adds mollieToCtOrderId and ctToMollieOrderId functions
    Extends Utils by adding the mocking of ctToMollieOrderId and mollieToCtOrderId functions

--- extension/src/requestHandlers/cancelOrder.ts
+++ extension/src/requestHandlers/cancelOrder.ts
@@ -5,12 +5,18 @@ import formatErrorResponse from '../errorHandlers';
 import { Action, ControllerAction, CTPayment, CTTransactionState, CTTransactionType, CTUpdatesRequestedResponse } from '../types';
 import { isPartialTransaction, findInitialTransaction, ctToMollieLines, mollieToCtLines } from '../utils';
 import { makeActions } from '../makeActions';
+import { ctToMollieOrderId } from '../utils';
 
 export function getCancelOrderParams(ctPayment: Required<CTPayment>, mollieOrder: Order | undefined): Promise<OrderLineCancelParams> {
+  Logger.debug('getCancelOrderParams : ctPayment : ' + JSON.stringify(ctPayment));
+  Logger.debug('getCancelOrderParams : mollieOrder : ' + JSON.stringify(mollieOrder));
   try {
     const initialCancelAuthorization = findInitialTransaction(ctPayment.transactions, CTTransactionType.CancelAuthorization);
+
+    let mollieOrderId = ctToMollieOrderId(ctPayment.key);
+
     const cancelOrderParams = {
-      orderId: ctPayment.key,
+      orderId: mollieOrderId,
       lines: ctToMollieLines(initialCancelAuthorization!, mollieOrder!.lines),
     };
 
@@ -48,11 +54,14 @@ export function createCtActions(mollieCancelOrderRes: Order | boolean, ctPayment
 }
 
 export default async function cancelOrder(ctPayment: Required<CTPayment>, mollieClient: MollieClient, getCancelOrderParams: Function, createCtActions: Function): Promise<CTUpdatesRequestedResponse> {
+  Logger.debug('cancelOrder : ctPayment : ' + JSON.stringify(ctPayment));
   try {
-    const mollieOrderRes = isPartialTransaction(ctPayment.transactions ?? [], CTTransactionType.CancelAuthorization) ? await mollieClient.orders.get(ctPayment.key) : undefined;
+    let mollieOrderId = ctToMollieOrderId(ctPayment.key);
+
+    const mollieOrderRes = isPartialTransaction(ctPayment.transactions ?? [], CTTransactionType.CancelAuthorization) ? await mollieClient.orders.get(mollieOrderId) : undefined;
     const cancelOrderParams = mollieOrderRes ? await getCancelOrderParams(ctPayment, mollieOrderRes) : undefined;
     Logger.debug('cancelOrderParams: %o', cancelOrderParams);
-    const mollieCancelOrderRes = cancelOrderParams ? await mollieClient.orders_lines.cancel(cancelOrderParams) : await mollieClient.orders.cancel(ctPayment.key);
+    const mollieCancelOrderRes = cancelOrderParams ? await mollieClient.orders_lines.cancel(cancelOrderParams) : await mollieClient.orders.cancel(mollieOrderId);
     Logger.debug('mollieCancelOrderRes: %o', mollieCancelOrderRes);
     const ctActions = createCtActions(mollieCancelOrderRes, ctPayment);
     return {
